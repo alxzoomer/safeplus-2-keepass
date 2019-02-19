47474e784e0fd3f7063b989fdedee9f5fb0a8f4a
Alexey Nikitin <alxzoomer@gmail.com>
2019-02-20 01:28:11 +0400

    Add linter

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-var parser = require('xml2json');
-var fs = require('fs')
-var process = require('process')
-var kdbxweb = require('kdbxweb')
+const parser = require('xml2json');
+const fs = require('fs');
+const process = require('process');
+const kdbxweb = require('kdbxweb');
 
-if (process.argv.length > 2) {
-  if (fs.existsSync(process.argv[2])) {
-    let xml = fs.readFileSync(process.argv[2])
-    json = parser.toJson(xml, {"object": true, "alternateTextNode": "_vv_"})
-    exportToKeepass(json.safeboxplus.folder)
-  } else {
-    console.log("File doesn't exists")
-  }
-} else {
-  console.log("Please specify existing file path")
+require('dotenv').config();
+
+const xmlFilePath = process.env.XML_FILE;
+function exportToKeepass(json) {
+  const credentials = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString('password123'));
+  const db = kdbxweb.Kdbx.create(credentials, 'Passwords Database');
+  const defaultGroup = db.getDefaultGroup();
+  json.forEach((group) => {
+    console.log(group.title);
+    const gr = db.createGroup(defaultGroup, group.title);
+    gr.notes = group.description;
+    console.log(group.card);
+    if (group.card instanceof Array) {
+      group.card.forEach((card) => {
+        console.log(JSON.stringify(card, undefined, 2));
+      });
+    } else {
+      console.log(JSON.stringify(group.card, undefined, 2));
+    }
+  });
+
+  db.save().then((value) => {
+    fs.writeFileSync(process.env.KDBX_FILE, Buffer.from(value));
+  });
 }
 
-function exportToKeepass(json) {
-  let credentials = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString("password123"))
-  let db = kdbxweb.Kdbx.create(credentials, 'Passwords Database');
-  defaultGroup = db.getDefaultGroup()
-  json.forEach(group => {
-    console.log(group.title);
-    subgroup = db.createGroup(defaultGroup, group.title);
-  });
-  db.save().then(value => {
-    fs.writeFileSync("safeplus.kdbx", Buffer.from(value))
-  });
+if (xmlFilePath) {
+  if (fs.existsSync(xmlFilePath)) {
+    const xml = fs.readFileSync(xmlFilePath);
+    const json = parser.toJson(xml, { object: true, alternateTextNode: '_vv_' });
+    exportToKeepass(json.safeboxplus.folder);
+  } else {
+    console.log(`File ${xmlFilePath} doesn't exists`);
+  }
+} else {
+  console.log('Please specify existing file path');
 }
