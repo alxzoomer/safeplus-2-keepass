8ff0d16cca7ffc6963eba48e18c7749ab687fd90
Alexey Nikitin <alexeyn@cqg.com>
2019-02-19 17:09:53 +0400

    Add kdbxweb lib

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # safeplus-2-keepass
-Safe+ XML DB converter to Keepass DB format
+
+[Safe+](https://play.google.com/store/apps/details?id=com.zholdak.safeboxpro) XML DB converter to Keepass DB format
+
+## Links
+
+* [Web Kdbx library](https://github.com/keeweb/kdbxweb)
+* [Converts XML to JSON using node-expat](https://github.com/buglabs/node-xml2json)
--- index.js
+++ index.js
@@ -4,19 +4,16 @@ var process = require('process')
 
 if (process.argv.length > 2) {
   if (fs.existsSync(process.argv[2])) {
-    var xml = fs.readFile(process.argv[2], (err, data) => {
-      if (err != null) {
-        console.err(err)
-      } else {
-        xml = data.toString()
-        console.log(xml)
-        json = parser.toJson(xml, {"object": true, "alternateTextNode": "_vv_"})
-        console.log(JSON.stringify(json, undefined, 2))
-      }
-    })
+    var xml = fs.readFileSync(process.argv[2])
+    json = parser.toJson(xml, {"object": true, "alternateTextNode": "_vv_"})
+    exportToKeepass(json.safeboxplus.folder)
   } else {
     console.log("File doesn't exists")
   }
 } else {
   console.log("Please specify existing file path")
+}
+
+function exportToKeepass(json) {
+  console.log(JSON.stringify(json, undefined, 2))
 }
