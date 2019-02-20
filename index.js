719a0d87187ea4d615b2e7a57f45e5466e0b792e
Alexey Nikitin <alxzoomer@gmail.com>
2019-02-20 16:18:15 +0400

    Refactoring

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const parser = require('xml2json');
 const fs = require('fs');
-const process = require('process');
 const kdbxweb = require('kdbxweb');
+const chalk = require('chalk');
+
+const { log } = console;
 
 require('dotenv').config();
 
-const xmlFilePath = process.env.XML_FILE;
 function exportToKeepass(json) {
   const credentials = new kdbxweb.Credentials(kdbxweb.ProtectedValue.fromString('password123'));
   const db = kdbxweb.Kdbx.create(credentials, 'Passwords Database');
   const defaultGroup = db.getDefaultGroup();
   json.forEach((group) => {
-    console.log(group.title);
+    log(`Group "${chalk.blue(group.title)}"`);
     const gr = db.createGroup(defaultGroup, group.title);
     gr.notes = group.description;
-    console.log(group.card);
+    // log(group.card);
     if (group.card instanceof Array) {
       group.card.forEach((card) => {
-        console.log(JSON.stringify(card, undefined, 2));
+        // log(JSON.stringify(card, undefined, 2));
       });
     } else {
-      console.log(JSON.stringify(group.card, undefined, 2));
+      // log(JSON.stringify(group.card, undefined, 2));
     }
   });
 
@@ -29,14 +30,19 @@ function exportToKeepass(json) {
   });
 }
 
-if (xmlFilePath) {
-  if (fs.existsSync(xmlFilePath)) {
-    const xml = fs.readFileSync(xmlFilePath);
-    const json = parser.toJson(xml, { object: true, alternateTextNode: '_vv_' });
-    exportToKeepass(json.safeboxplus.folder);
+function main() {
+  const xmlFilePath = process.env.XML_FILE;
+  if (xmlFilePath) {
+    if (fs.existsSync(xmlFilePath)) {
+      const xml = fs.readFileSync(xmlFilePath);
+      const json = parser.toJson(xml, { object: true, alternateTextNode: '_vv_' });
+      exportToKeepass(json.safeboxplus.folder);
+    } else {
+      log(`File ${xmlFilePath} doesn't exists`);
+    }
   } else {
-    console.log(`File ${xmlFilePath} doesn't exists`);
+    log('Please specify existing file path');
   }
-} else {
-  console.log('Please specify existing file path');
 }
+
+main();
